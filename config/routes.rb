de612650d3dc297444c04a548adc140aa7c574f7
Ungureanu Bogdan <ungureanu.bogdan.sorin@gmail.com>
2017-01-15 19:40:05 +0200

    add some layout elements

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
   devise_for :users
-  root 'chat_rooms#index'
+  root 'home#index'
 
   resources :chat_rooms, only: [:new, :create, :show, :index]
 end
